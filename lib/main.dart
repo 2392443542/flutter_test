97ea7ae3f019ecfca06461ad83c2e463c4137180
alieen lee <alieen2392443542@gmail.com>
2021-04-02 15:07:15 +0800

    lwj_add:InheritedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'TestWidget.dart';
+import 'ShareDataWidget.dart';
 
 void main() {
   runApp(MyApp());
@@ -50,20 +52,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  List<Widget> itemList = List();
+  int count = 0;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    for (var i = 0; i < 9; i++) {
-      itemList.add(item());
-      // print(itemList);
-    }
-  }
-
-  void _incrementCounter() {
-    setState(() {});
   }
 
   @override
@@ -74,122 +66,26 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: ListView(
-        children: itemList,
-      ),
-    );
-  }
-
-  Widget item() {
-    return Container(
-      margin: EdgeInsets.all(10),
-      padding: EdgeInsets.all(10),
-      height: 90,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.all(Radius.circular(16.0)),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.black12, offset: Offset(0, 5), blurRadius: 1.0),
-        ],
-      ),
-      child: Row(
-        children: <Widget>[
-          Container(
-            width: 50,
-            height: 50,
-            color: Colors.red,
-            alignment: Alignment.centerLeft,
+      body: Center(
+        child: ShareDataWidget(
+          data: count,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: TestWidget(),
+              ),
+              RaisedButton(
+                child: Text("Increment"),
+                //每点击一次，将count自增，然后重新build,ShareDataWidget的data将被更新
+                onPressed: () => setState(() {
+                  ++count;
+                  print(count);
+                }),
+              )
+            ],
           ),
-          detailWidget(),
-          Container(
-            width: 60,
-            height: 60,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1.0, color: Colors.black12),
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              // color: Colors.red,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('单'), Text('赔1.91')],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 10),
-            width: 60,
-            height: 60,
-
-            // color: Colors.purple,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(5.0)),
-              border: Border.all(width: 1.0, color: Colors.black12),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Text('双'), Text('赔1.91')],
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget detailWidget() {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.only(left: 15, right: 15),
-        // color: Colors.green,
-        alignment: Alignment.center,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "打发pk",
-              textAlign: TextAlign.left,
-            ),
-            Text("123456期", textAlign: TextAlign.left),
-            Row(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    color: Colors.black12,
-                  ),
-                  child: Text(
-                    "第四",
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5),
-                  margin: EdgeInsets.only(left: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    color: Colors.green,
-                  ),
-                  child: Text(
-                    "双",
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(5),
-                  margin: EdgeInsets.only(left: 5),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    color: Colors.red,
-                  ),
-                  child: Text(
-                    "8期",
-                    style: TextStyle(color: Colors.white, fontSize: 12),
-                  ),
-                ),
-              ],
-            ),
-          ],
         ),
       ),
     );
